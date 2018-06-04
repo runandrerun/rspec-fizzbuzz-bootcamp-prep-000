f0d31aabafc4d60f02c83526ae37e01ce2f6b2c0
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-04 20:57:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-    puts "Fizz"
+  if int % 3 && int % 5 == 0
+    puts "Fizzbuzz"
   elsif int % 5 == 0
     puts "Buzz"
-  elsif int % 3 && int % 5 == 0
-    puts "fizzbuzz"
+  elsif int % 3 == 0
+    puts "Fizz"
   else 
     nil
   end
